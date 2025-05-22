31b182f9c65ceac8315e020ef96e1cdf1a619c4d
JeongDay <cidd1332@gmail.com>
2025-05-22 11:57:35 +0900

    GameObject Find, Component

--- Assets/02. Scripts/StudyComponent.cs
+++ Assets/02. Scripts/StudyComponent.cs
@@ -4,15 +4,25 @@ public class StudyComponent : MonoBehaviour
 {
     public GameObject obj;
 
-    public string changeName;
-    
+    public Mesh msh;
+    public Material mat;
     
     void Start()
     {
-        obj = GameObject.Find("Main Camera"); // Main Camera 오브젝트를 찾아서 할당하는 기능
+        CreateCube();
+    }
+
+    public void CreateCube()
+    {
+        obj = new GameObject("Cube");
         
-        obj.name = changeName;
+        obj.AddComponent<MeshFilter>();
+        obj.GetComponent<MeshFilter>().mesh = msh;
         
+        obj.AddComponent<MeshRenderer>();
+        obj.GetComponent<MeshRenderer>().material = mat;
+        
+        obj.AddComponent<BoxCollider>();
     }
     
 }
